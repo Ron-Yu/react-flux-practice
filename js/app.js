2a7b9b015a5eb95c8cf20a9bc84f75c8c2de76ee
Ron Yu <ronyu77@gmail.com>
2016-03-21 11:21:04 +0800

    test git

--- js/app.js
+++ js/app.js
@@ -2,3 +2,15 @@ React.render(
     <Forum />,
     document.getElementById('forum')
 );
+
+var myemitter = new EventEmitter();
+
+myemitter.on('STARTED_THE_APP', function () {
+    console.log('start the app');
+});
+
+myemitter.on('STARTED_THE_APP', function () {
+    console.log('start the app #2');
+});
+
+myemitter.emit('STARTED_THE_APP');
